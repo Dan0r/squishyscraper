a474e77ff30e9979bc216e193367e0d25d35d1f3
furukawa <dkszoeke@outlook.de>
2025-04-05 23:10:42 +0200

    found shadowdom
    navigated into shadowdom to click cookie, searched for the shoe

--- scraper.py
+++ scraper.py
@@ -1,8 +1,14 @@
 import os
 import requests
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 
 # set-up for WSL2
 ## Setup chrome options
@@ -16,10 +22,27 @@ chrome_options.binary_location = f"{homedir}/chrome-linux64/chrome"
 webdriver_service = Service(f"{homedir}/chromedriver-linux64/chromedriver")
 
 # Choose Chrome Browser
-browser = webdriver.Chrome()
-url = "https://www.zalando.de"
-browser.get(url)
-title = browser.title
-print(f"Der Titel der URL '{url}' lautet: '{title}'")
+driver = webdriver.Chrome()
 
-#description = browser.find_element(By.NAME, "description").get_attribute("content")
+# Open Zalando
+url = "https://www.zalando.de"
+shoe = "Asics Japan S"
+
+driver.get(url)
+# Navigate into Shadow DOM to select cookie-button 
+try:
+    time.sleep(3)
+    cookie = driver.execute_script('return document.querySelector("#usercentrics-root").shadowRoot.querySelector("[data-testid=\'uc-deny-all-button\']")')
+    cookie.click()
+    print("cookie was clicked")
+except:
+    print("cookie not clicked")
+# Search for Asics Japan S 
+searchbox = driver.find_element("id","header-search-input")
+searchbox.click()
+
+searchbox.send_keys(shoe)
+searchbox.send_keys(Keys.ENTER)
+
+time.sleep(3)
+driver.quit()
